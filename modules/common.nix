841b0fabfe390dffbf1b799157930936335a68b6
AGawas <arnsg730@proton.me>
2026-05-04 21:43:08 -0400

    add packges

--- modules/common.nix
+++ modules/common.nix
@@ -2,16 +2,46 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
-
   users.users.zxcv = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" ];
     shell = pkgs.zsh;
   };
-
   programs.zsh.enable = true;
-
   environment.systemPackages = with pkgs; [
-    git curl wget ripgrep fd
+    git
+    curl
+    wget
+    ripgrep
+    fd
+    htop
+    btop
+    dust
+    duf
+    lsof
+    pciutils
+    usbutils
+    smartmontools
+    nethogs
+    iftop
+    unzip
+    p7zip
+    tmux
+    tree
+    jq
+    strace
+    gcc
+    gnumake
+    cmake
+    python3
+    nodejs
+    rustup
+    go
+    docker-compose
+    direnv
+    gh
+    just
+    sqlite
+    httpie
   ];
 }
